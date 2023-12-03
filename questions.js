b02413f22b7ecffb91a376c26872bdc921d36b8a
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2023-12-03 19:01:54 +0530

    Final Commit

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the capital of France?",
+        options: [
+            "Paris", 
+            "Madrid", 
+            "Rome", 
+            "Berlin"
+        ],
+        answer: "Paris"
+      },
+      {
+        numb: 2,
+        question: "Which planet is known as the Red Planet?",
+        options: [
+            "Venus", 
+            "Mars", 
+            "Jupiter", 
+            "Saturn"
+        ],
+        answer: "Mars"
+      },
+      {
+        numb: 3,
+        question: "What is the largest mammal?",
+        options: [
+            "Elephant", 
+            "Blue Whale", 
+            "Giraffe", 
+            "Hippopotamus"
+        ],
+        answer: "Blue Whale"
+      },
+      {
+        numb: 4,
+        question: "Who wrote 'Romeo and Juliet'?",
+        options: [
+        "Charles Dickens", 
+        "Jane Austen", 
+        "William Shakespeare", 
+        "Mark Twain"
+        ],
+        answer: "William Shakespeare"
+      },
+      {
+        numb: 5,
+        question: "What is the capital of Japan?",
+        options: [
+        "Seoul", 
+        "Beijing", 
+        "Tokyo", 
+        "Bangkok"
+        ],
+        answer: "Tokyo"
+      },
+      {
+        numb: 6,
+        question: "Which country is known as the Land of the Rising Sun?",
+        options: [
+        "China", 
+        "Japan", 
+        "Korea", 
+        "Vietnam"
+        ],
+        answer: "Japan"
+      },
+      {
+        numb: 7,
+        question: "What is the currency of India?",
+        options: [
+        "Rupee", 
+        "Yuan", 
+        "Rupiah", 
+        "Ringgit"
+        ],
+        answer: "Rupee"
+      },
+      {
+        numb: 8,
+        question: "In which year did the Titanic sink?",
+        options: [
+        "1912", 
+        "1920", 
+        "1931", 
+        "1945"
+        ],
+        answer: "1912"
+      },
+      {
+        numb: 9,
+        question: "Who painted the Mona Lisa?",
+        options: [
+        "Pablo Picasso", 
+        "Vincent van Gogh", 
+        "Leonardo da Vinci", 
+        "Claude Monet"
+        ],
+        answer: "Leonardo da Vinci"
+      },
+      {
+        numb: 10,
+        question: "What is the largest ocean on Earth?",
+        options: [
+            "Atlantic Ocean", 
+            "Indian Ocean", 
+            "Southern Ocean", 
+            "Pacific Ocean",
+        ],
+        answer: "Pacific Ocean"
+      },
+];
